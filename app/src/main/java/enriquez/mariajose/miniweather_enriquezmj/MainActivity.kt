d42d63abb35147cd0814755d6065ea4f41448c9e
Majo8805 <maria.enriquez252337@potros.itson.edu.mx>
2026-09-07 22:12:39 -0700

    Initial commit

---
+++ app/src/main/java/enriquez/mariajose/miniweather_enriquezmj/MainActivity.kt
@@ -0,0 +1,29 @@
+package enriquez.mariajose.miniweather_enriquezmj
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val tvGreeting = findViewById<TextView>(R.id.tvGreeting)
+        val tvCity = findViewById<TextView>(R.id.tvCity)
+        val ivWeather = findViewById<ImageView>(R.id.ivWeather)
+        val tvTemperature = findViewById<TextView>(R.id.tvTemperature)
+        val tvWeather = findViewById<TextView>(R.id.tvWeather)
+
+    }
+}
